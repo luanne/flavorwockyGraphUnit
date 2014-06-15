997228c310756c2636318ff5e9da44c5f46ccee4
Luanne Misquitta <luanne.coutinho@gmail.com>
2014-06-15 21:40:24 +0530

    Check that a duplicate ingredient is not created

--- src/test/java/com/flavorwocky/domain/pairing/PairingTest.java
+++ src/test/java/com/flavorwocky/domain/pairing/PairingTest.java
@@ -11,7 +11,9 @@ import org.junit.Test;
 public class PairingTest extends BaseTest {
 
     @Test
-    public void testSavePairing() {
+    public void testSaveFirstPairing() {
+
+        //Check that the first pairing in the graph is created correctly
 
         Ingredient ing1 = new Ingredient("Chicken");
         Ingredient ing2 = new Ingredient("Garlic");
@@ -25,29 +27,42 @@ public class PairingTest extends BaseTest {
 
         GraphUnit.assertSameGraph(getGraphDb(), ingredientSubgraph);
 
+    }
 
-        ing1 = new Ingredient("Lemon");
-        ing2 = new Ingredient("Garlic");
-        pairing = new Pairing();
+    @Test
+    public void testSavePairingWithExistingIngredient() {
+        Ingredient ing1 = new Ingredient("Lemon");
+        Ingredient ing2 = new Ingredient("Garlic");
+        Pairing pairing = new Pairing();
         pairing.setFirstIngredient(ing1);
         pairing.setSecondIngredient(ing2);
         pairing.setAffinity(Affinity.TRIED_TESTED);
         pairing.save();
 
-        ingredientSubgraph = "create (i1:Ingredient {name: 'Lemon'}), (i2:Ingredient {name: 'Garlic'}), (i1)<-[:hasIngredient]-(p:Pairing {affinity: 0.6, allAffinities:[0.6]}), (p)-[:hasIngredient]->(i2) merge (i1)-[:pairsWith]-(i2)";
+        String ingredientSubgraph = "create (i1:Ingredient {name: 'Lemon'}), (i2:Ingredient {name: 'Garlic'}), (i3:Ingredient {name: 'Chicken'})," +
+                " (i1)<-[:hasIngredient]-(p:Pairing {affinity: 0.6, allAffinities:[0.6]})-[:hasIngredient]->(i2) , " +
+                "(i2)<-[:hasIngredient]-(p2:Pairing {affinity: 0.45, allAffinities:[0.45]})-[:hasIngredient]->(i3) " +
+                " merge (i1)-[:pairsWith]-(i2) merge (i3)-[:pairsWith]-(i2)";
 
-        GraphUnit.assertSubgraph(getGraphDb(), ingredientSubgraph);
+        GraphUnit.assertSameGraph(getGraphDb(), ingredientSubgraph);
 
-        ing1 = new Ingredient("Lemon");
-        ing2 = new Ingredient("Garlic");
-        pairing = new Pairing();
+
+    }
+
+    @Test
+    public void testUpdatePairing() {
+        Ingredient ing1 = new Ingredient("Lemon");
+        Ingredient ing2 = new Ingredient("Garlic");
+        Pairing pairing = new Pairing();
         pairing.setFirstIngredient(ing1);
         pairing.setSecondIngredient(ing2);
         pairing.setAffinity(Affinity.EXTREMELY_GOOD);
         pairing.save();
 
-        ingredientSubgraph = "create (i1:Ingredient {name: 'Lemon'}), (i2:Ingredient {name: 'Garlic'}), (i1)<-[:hasIngredient]-(p:Pairing {affinity: 0.6, allAffinities:[0.6,0.45]}), (p)-[:hasIngredient]->(i2) merge (i1)-[:pairsWith]-(i2)";
+        String ingredientSubgraph = "create (i1:Ingredient {name: 'Lemon'}), (i2:Ingredient {name: 'Garlic'}), (i1)<-[:hasIngredient]-(p:Pairing {affinity: 0.6, allAffinities:[0.6,0.45]}), (p)-[:hasIngredient]->(i2) merge (i1)-[:pairsWith]-(i2)";
 
         GraphUnit.assertSubgraph(getGraphDb(), ingredientSubgraph);
+
     }
+
 }
